83143c76ef4396246039932f121ff563910c86e0
Md. Mahdi Mohtasim <70072135+Mahdi3M@users.noreply.github.com>
2023-09-12 16:09:00 +0600

    Revert "Temp"
    This reverts commit 4ccb1f0611aaf8b4a29cbbaff8c4876c2f89ed95.

--- PyQt Application/main.py
+++ PyQt Application/main.py
@@ -13,8 +13,10 @@ class MyWindow(QMainWindow):
         
         # Define Widgets...
         central_widget = self.centralwidget
+        central_layout = self.verticalLayout
         
         # Edit widget UI...
+        central_widget.setLayout(central_layout)
 
 
 
