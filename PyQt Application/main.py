d9c5c648f8a5c5ec4d3765d5f6fccf235d371509
Md. Mahdi Mohtasim <70072135+Mahdi3M@users.noreply.github.com>
2023-09-26 08:55:55 +0600

    -Created Stacked Widget -Created Camera and Keyboard Page -Added Toolbar

--- PyQt Application/main.py
+++ PyQt Application/main.py
@@ -11,10 +11,19 @@ class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         loadUi("interface.ui", self)
+        self.video = FaceMeshWidget()
+        self.calibrate()
         
         # Define Widgets...
-        central_widget = self.centralwidget
+        menu_camera_button = self.actionCamera
+        menu_keyboard_button = self.actionKeyboard
+               
         self.video_layout= self.findChild(QVBoxLayout, 'verticalLayout_3')
+        self.pages = self.findChild(QStackedWidget, 'stackedWidget')
+        
+        self.camera_page = self.findChild(QWidget, 'cameraPage')
+        self.keyboard_page = self.findChild(QWidget, 'keyboardPage')
+        
         self.start_video_button = self.findChild(QPushButton, 'startVideoButton')
         self.end_video_button = self.findChild(QPushButton, 'endVideoButton')
         self.calibrate_button = self.findChild(QPushButton, 'calibrateButton')
@@ -24,12 +33,15 @@ class MyWindow(QMainWindow):
         self.end_video_button.hide()
         
         # Button Controls
+        menu_camera_button.triggered.connect(self.startVideo)
+        menu_keyboard_button.triggered.connect(self.startTyping)
+        
         self.start_video_button.clicked.connect(self.startVideo)
         self.end_video_button.clicked.connect(self.endVideo)
         self.calibrate_button.clicked.connect(self.calibrate)
 
     def startVideo(self):
-        self.video = FaceMeshWidget()
+        self.pages.setCurrentWidget(self.camera_page)
         self.video_layout.addWidget(self.video)
         self.start_video_button.hide()
         self.calibrate_button.show()
@@ -49,6 +61,9 @@ class MyWindow(QMainWindow):
         self.start_video_button.show()
         self.calibrate_button.hide()
         self.end_video_button.hide()
+        
+    def startTyping(self):
+        self.pages.setCurrentWidget(self.keyboard_page)
         
         
     
